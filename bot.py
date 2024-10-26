28c9942b4071d4d278fc77ff1d488483a12b7813
CryptoChristian12 <tatesempire2.0@gmail.com>
2024-10-27 01:34:36 +0200

    Update and rename bot to bot.py

---
+++ bot.py
@@ -0,0 +1,115 @@
+!/usr/bin/env python
+
+import webbrowser
+import time
+
+logo = f"""
+
+$$$$$$$$\ $$$$$$$\   $$$$$$\  $$$$$$$$\ $$$$$$$$\ $$$$$$\  $$$$$$\         $$$$$$\  $$$$$$$$\ $$\   $$\ $$$$$$$\  $$$$$$$$\ $$$$$$$\  
+\__$$  __|$$  __$$\ $$  __$$\ $$  _____|$$  _____|\_$$  _|$$  __$$\       $$  __$$\ $$  _____|$$$\  $$ |$$  __$$\ $$  _____|$$  __$$\ 
+   $$ |   $$ |  $$ |$$ /  $$ |$$ |      $$ |        $$ |  $$ /  \__|      $$ /  \__|$$ |      $$$$\ $$ |$$ |  $$ |$$ |      $$ |  $$ |
+   $$ |   $$$$$$$  |$$$$$$$$ |$$$$$\    $$$$$\      $$ |  $$ |            \$$$$$$\  $$$$$\    $$ $$\$$ |$$ |  $$ |$$$$$\    $$$$$$$  |
+   $$ |   $$  __$$< $$  __$$ |$$  __|   $$  __|     $$ |  $$ |             \____$$\ $$  __|   $$ \$$$$ |$$ |  $$ |$$  __|   $$  __$$< 
+   $$ |   $$ |  $$ |$$ |  $$ |$$ |      $$ |        $$ |  $$ |  $$\       $$\   $$ |$$ |      $$ |\$$$ |$$ |  $$ |$$ |      $$ |  $$ |
+   $$ |   $$ |  $$ |$$ |  $$ |$$ |      $$ |      $$$$$$\ \$$$$$$  |      \$$$$$$  |$$$$$$$$\ $$ | \$$ |$$$$$$$  |$$$$$$$$\ $$ |  $$ |
+   \__|   \__|  \__|\__|  \__|\__|      \__|      \______| \______/        \______/ \________|\__|  \__|\_______/ \________|\__|  \__|
+                                                                                                                                      
+                                                                                                                                      
+"""
+
+print(logo)
+
+url = int(input("What is your url? "))
+
+wait_time = int(input("What's the waittime? ")
+
+x = 1
+while True:
+  webbrowser.open(url)
+  time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+        webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+        webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)	
+	webbrowser.open_new_tab(url)
+	time.sleep(wait_time)
+	x +=1
+	
+
+
+
+	
